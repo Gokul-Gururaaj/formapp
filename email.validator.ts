552c644a36e4c4f611ce29383f50b00ef19e2b93
Gokul-Gururaaj <67002314+Gokul-Gururaaj@users.noreply.github.com>
2022-05-17 16:51:13 +0530

    Update email.validator.ts

--- email.validator.ts
+++ email.validator.ts
@@ -1,3 +1,5 @@
+
+   
 import { AbstractControl } from "@angular/forms"; 
 
 // export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
@@ -6,10 +8,28 @@ import { AbstractControl } from "@angular/forms";
 //       return forbidden ? {forbiddenName: {value: control.value}} : null;
 //     };
 //   }
-  export function EmailValidator(control: AbstractControl): {[key:string]:any} |null
+ 
+    export function EmailValidator(control: AbstractControl): {[key:string]:any} |null
     {
-      const forbidden = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(control.value);
-      return forbidden ? null : {'forbiddenName': {value: control.value}};
+        const emailname=control.value;
+    
+        const receiptantname   = emailname.substring(0, emailname.lastIndexOf("@"));
+        
+        const domainname = emailname.substring(emailname.indexOf("@") +1);
+        const cdomainname = domainname.substring(0,domainname.indexOf("."));
+        const extensionname=domainname.substring(domainname.indexOf(".") +1);  
+        const forbidden = /^[a-zA-Z0-9_.+-]+@[a-z]+\.[a-z.]+$/.test(emailname);
+      const receiptantnamev = /[a-zA-Z0-9_.+-]/.test(receiptantname);
+      const dlist=[/^gmail$/,/^yahoo$/,/^hotmail$/,/^ritchennai$/,/^elait$/] 
+      const domainnamev= dlist.some(name => name.test(cdomainname));
+      const elist=[/^com$/,/^co.in$/,/^edu.in$/] 
+      
+      
+      
+      const extensionnamev= elist.some(name => name.test(extensionname));
+      
+      return forbidden && receiptantnamev && domainnamev &&  extensionnamev ? null : {'forbiddenName': {value: control.value}};
     };
+
 
   
